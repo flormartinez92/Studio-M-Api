a45ea14694580e674172bbd8075c282ec50cb8d2
IvoDev28 <ivanlucana6@gmail.com>
2023-09-27 15:47:51 -0300

    server-configuration-finalized

--- api/config/db.js
+++ api/config/db.js
@@ -1,1 +1,16 @@
+const moongose = require("mongoose");
+
 // Configuracion DB
+const dbConnection = async () => {
+  try {
+    await moongose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+    });
+    console.log("Base de datos online");
+  } catch (error) {
+    console.log(error);
+    throw new Error("Error al iniciar la base de datos");
+  }
+};
+
+module.exports = { dbConnection };
---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const { dbConnection } = require("./config/db");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: `${process.env.STUDIO_M_CLIENT_HOST}`,
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
+dbConnection();
+
+app.listen(process.env.PORT, () =>
+  console.log(`listening on port ${process.env.PORT}`)
+);
