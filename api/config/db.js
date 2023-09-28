059b336f8d03f40b7ef2bd2f81858a5737187357
Rafael Mojica <90728588+RafaMojica@users.noreply.github.com>
2023-09-28 01:06:58 -0500

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -3,14 +3,16 @@ const moongose = require("mongoose");
 // Configuracion DB
 const dbConnection = async () => {
   try {
-    await moongose.connect(process.env.MONGO_URI, {
+    return await moongose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
-    console.log("online database");
   } catch (error) {
-    console.log(error);
-    throw new Error("error connecting to the database");
+    console.error("Failed to connect to database", error);
   }
 };
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to database");
+});
 
 module.exports = { dbConnection };
