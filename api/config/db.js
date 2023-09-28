7ac0a50b923da69e57a075379c917449ba12fd66
IvoDev28 <ivanlucana6@gmail.com>
2023-09-28 16:41:41 -0300

    routes-auth-user-finalized

--- api/config/db.js
+++ api/config/db.js
@@ -1,16 +1,1 @@
-const moongose = require("mongoose");
-
 // Configuracion DB
-const dbConnection = async () => {
-  try {
-    await moongose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-    });
-    console.log("online database");
-  } catch (error) {
-    console.log(error);
-    throw new Error("error connecting to the database");
-  }
-};
-
-module.exports = { dbConnection };
--- api/config/token.js
+++ api/config/token.js
@@ -1,12 +1,1 @@
 // Configuracion JWT
-const jwt = require("jsonwebtoken");
-
-const generateToken = (data) => {
-  return jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "2h" });
-};
-
-const validateToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
-};
-
-module.exports = { generateToken, validateToken };
