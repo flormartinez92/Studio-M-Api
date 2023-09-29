f7947c18263790ab59acefb36c5d7479dc3af313
Rafael Mojica <90728588+RafaMojica@users.noreply.github.com>
2023-09-28 20:25:52 -0500

    Update middleware auth.js

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -3,8 +3,17 @@ const { validateToken } = require("../config/token");
 const validateUser = (req, res, next) => {
   const { token } = req.cookies;
   try {
+    
+    if (!token) {
+      return res.status(401).json({ "Token de autenticación faltante" });
+    }
+    
     const payload = validateToken(token);
-    //console.log(payload);
+    
+    if (!payload) {
+      return res.status(401).json({ "Invalid authorization token" });
+    }
+    
     req.user = payload;
     next();
   } catch (error) {
