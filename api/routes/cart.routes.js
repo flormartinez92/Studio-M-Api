b8d85f17dc87a26e23e55876420164cc5f08d12e
flormartinez92 <martinez.mf.92@gmail.com>
2023-10-11 16:38:37 -0300

    create user-courses-route

--- api/routes/cart.routes.js
+++ api/routes/cart.routes.js
@@ -5,21 +5,20 @@ const { check } = require("express-validator");
 const validateFields = require("../middleware/validateFields.middleware");
 
 // Agrear Curso al carrito de compra
-router.post("/add/:courseId/:userId", cartController.add)
+router.post("/add/:courseId/:userId", cartController.add);
 
 // Eliminar producto de carrito de compra
-router.delete("/remove/:courseId/:userId",
-[
-  check("user", "User is required").not().isEmpty(),
-  check("course", "Course is required").not().isEmpty(),
-  validateFields
-], cartController.remove)
+router.delete(
+  "/remove/:courseId/:userId",
+  [
+    check("user", "User is required").not().isEmpty(),
+    check("course", "Course is required").not().isEmpty(),
+    validateFields,
+  ],
+  cartController.remove
+);
 
 // Confirmacion de Compra
-router.post("/confirmBuy/:userId",
-[
-  check("user", "User is required").not().isEmpty(),
-  validateFields
-], cartController.confirmBuy)
+router.post("/confirmBuy/:userId", cartController.confirmBuy);
 
 module.exports = router;
