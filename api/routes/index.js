1ab8e9958b3dcee4bc5a44f5d7d6929fbde6fb75
Nicolas Fridman <nicofri2001@gmail.com>
2023-11-02 18:39:50 -0300

    fixing

--- api/routes/index.js
+++ api/routes/index.js
@@ -5,39 +5,19 @@ const cart = require("./cart.routes");
 const course = require("./course.routes");
 const adminCourse = require("./adminCourse.routes");
 const adminUser = require("./adminUser.routes");
-<<<<<<< HEAD
-<<<<<<< HEAD
-const favorites = require("./favorites.routes");
-=======
-const adminCoupon = require("./adminCoupon.routes");
-const project = require("./project.routes");
-const adminProject = require("./adminProject.routes");
->>>>>>> b506f9859ba42ba7a303018489734d9e7767d979
-=======
 const favorites = require("./favorites.routes");
 const adminCoupon = require("./adminCoupon.routes");
 const project = require("./project.routes");
 const adminProject = require("./adminProject.routes");
->>>>>>> 8d9b350ea2f4f390a3d2958abfa07b3f7bcf056e
 
 router.use("/user", user);
 router.use("/cart", cart);
 router.use("/course", course);
 router.use("/adminCourse", adminCourse);
 router.use("/adminUser", adminUser);
-<<<<<<< HEAD
-<<<<<<< HEAD
-router.use("/favorites", favorites);
-=======
-router.use("/adminCoupon", adminCoupon);
-router.use("/project", project);
-router.use("/adminProject", adminProject);
->>>>>>> b506f9859ba42ba7a303018489734d9e7767d979
-=======
 router.use("/favorites", favorites);
 router.use("/adminCoupon", adminCoupon);
 router.use("/project", project);
 router.use("/adminProject", adminProject);
->>>>>>> 8d9b350ea2f4f390a3d2958abfa07b3f7bcf056e
 
 module.exports = router;
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -47,11 +47,13 @@ router.post("/forgot", validateForgotPassword, validateFields, forgotPassword);
 // ResetPassword
 router.post("/resetPassword", validateResetPassword, validateFields, resetPassword);
 
+//Ruta para traer los cursos comprados por un usuario
+router.post("/userCourses", validateEmail, validateFields, userCourses);
+
 //Ruta que trae la informacion del usuario
 router.get("/:userId", validateMongoID, validateFields, userData);
 
-//Ruta para traer los cursos comprados por un usuario
-router.get("/:userId/courses", validateMongoID, userCourses);
+// router.get("/:userId/courses", validateMongoID, userCourses);
 
 // Ruta para actualizar el estado de la clase
 router.put(
@@ -90,26 +92,13 @@ router.put(
   ],
   updateUser
 );
-<<<<<<< HEAD
 
 // Esta ruta deberia ser del administrador
 
 //router.get("/:userId/purchasedCourse", userCourses);
 
-<<<<<<< HEAD
-module.exports = router;
-=======
-
-router.get("/userCourses", validateEmail, validateFields, userCourses);
-
 // Ruta que me traiga los certificados del usuario
 router.get("/certificate", validateEmail, validateFields, allCertificates);
 
-//RUTA QUE TRAE LA INFO DEL USUARIO
-router.get("/:userId", validateMongoID, validateFields, userData);
 
 module.exports = router;
->>>>>>> b506f9859ba42ba7a303018489734d9e7767d979
-=======
-module.exports = router;
->>>>>>> 8d9b350ea2f4f390a3d2958abfa07b3f7bcf056e
