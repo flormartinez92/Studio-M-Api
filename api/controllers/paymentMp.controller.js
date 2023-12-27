ae7106ca09102924fff0a3ca445d402a1c14b6b3
Victoria Canclini <vico_canclini@hotmail.com>
2023-12-27 12:33:42 -0300

    avance rutas mp

---
+++ api/controllers/paymentMp.controller.js
@@ -0,0 +1,44 @@
+const mercadopago = require("mercadopago");
+const { MercadoPagoConfig, Preference } = require("mercadopago");
+
+exports.createOrder = async (req, res) => {
+  // const { title, price } = req.body;
+  const client = new MercadoPagoConfig({
+    accessToken: process.env.ACCESS_TOKEN_MP,
+  });
+
+  try {
+    const preference = new Preference(client);
+    const result = await preference.create({
+      body: {
+        items: [
+          {
+            title: "curso",
+            quantity: 1,
+            unit_price: 100,
+          },
+        ],
+        back_urls: {
+          success: "http://localhost:3000/success",
+          failure: "http://localhost:3000/failure",
+          pending: "http://localhost:3000/pending",
+        },
+        auto_return: "approved",
+      },
+    });
+
+    console.log(result);
+    res.json({ result });
+  } catch (error) {
+    console.error("Error creating preference:", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+exports.receiveWebhook = async (req, res) => {
+  const payment = req.query;
+  if (payment.type === "payment") {
+    const data = await mercadopago.Payment.findById(payment["data.id"]);
+  }
+  res.send("webhook");
+};
