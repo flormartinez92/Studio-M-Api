37d9330215a42fd889a5e6d0a1cbe52d3fe00eeb
Nicolas Fridman <nicofri2001@gmail.com>
2024-01-04 13:14:51 -0300

    cleaning Payment-MP-controller

--- api/controllers/paymentMp.controller.js
+++ api/controllers/paymentMp.controller.js
@@ -3,39 +3,27 @@ const { Order, Cart } = require("../models");
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 
 exports.createOrder = async (req, res) => {
-  //orderId title price
   const { cartCourses, orderId } = req.body;
+
   try {
-
-
+    //Busca el modelo de la orden actual en la DB
     const orderResponse = await Order.findById(orderId);
-    // console.log(orderResponse);
-    if (!orderResponse) {
-      // no se encontro la orden
-      return res.sendStatus(500);
-    }
+    !orderResponse && res.sendStatus(500);
 
     // Obtén todos los items del carrito asociados a la orden
     const cartItems = await Cart.findOne({ userId: orderResponse.userId });
 
-    // Calcula el total amount sumando los precios de los items del carrito
-    const totalAmount = cartItems.totalAmount;
-    // Obtén descuento del carrito asociados a la orden
+    // Obtén descuento del carrito asociados a la orden (de haberlo aplicado)
     const totalDiscount = cartItems.discount;
 
-    // Si hay un descuento, réstalo del totalAmount
-    // const totalDiscount = cartItems.totalDiscount || 0;
-    // const discountedTotalAmount = totalAmount + totalDiscount;
-
-    const client = new MercadoPagoConfig({
-      accessToken: process.env.ACCESS_TOKEN_MP,
-    });
+    //Genera configuracion de Mercado Pago
+    const client = new MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN_MP });
 
     try {
-
+      //Se crea un arreglo para guardar las ordenes creadas en base a la cantidad de cursos en el carrito
       const courses = [];
       cartCourses.forEach(element => {
-        //manejando decuentos
+        //Manejando decuentos
         const price = parseFloat(element.coursePrice);
         const discount = (price * totalDiscount) / 100;
         const finalDiscount = price - discount; 
@@ -48,17 +36,8 @@ exports.createOrder = async (req, res) => {
         courses.push(order);
       });
 
-
-
-
+      //Preferencias y creacion para Mercado Pago
       const preference = new Preference(client);
-      // const priceNumber = parseFloat(price);
-      //decuento
-      // const discount = (priceNumber * totalDiscount) / 100;
-      // const finalDiscount =  priceNumber - discount
-      // unit_price: totalDiscount ? finalDiscount : priceNumber,
-
-      // console.log("--------------------------------", finalDiscount);
       const result = await preference.create({
         body: {
           items: courses,
@@ -70,7 +49,7 @@ exports.createOrder = async (req, res) => {
           auto_return: "approved",
         },
       });
-      console.log(result);
+
       orderResponse.mpPreferenceID = result.id;
       await orderResponse.save();
       res.send(orderResponse).status(200);
@@ -79,7 +58,7 @@ exports.createOrder = async (req, res) => {
       res.status(500).send("Internal Server Error");
     }
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.sendStatus(500);
   }
 };
