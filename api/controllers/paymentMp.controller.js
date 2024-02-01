ed453fa96451c9257dfa1dd3455c4dcf6bfda8eb
IvoDev28 <ivanlucana6@gmail.com>
2024-02-01 02:53:50 -0300

    finalized

--- api/controllers/paymentMp.controller.js
+++ api/controllers/paymentMp.controller.js
@@ -2,9 +2,54 @@ const mercadopago = require("mercadopago");
 const { Order, Cart } = require("../models");
 const { MercadoPagoConfig, Preference } = require("mercadopago");
 const { findOne } = require("../models/user.models");
+const client = new MercadoPagoConfig({
+  accessToken: process.env.ACCESS_TOKEN_MP,
+});
+
+exports.createOrderIvan = async (req, res) => {
+  const { quantity, id, title, price } = req.body;
+  console.log({ quantity, id, title, price });
+  try {
+    const preference = new Preference(client);
+
+    const response = await preference.create({
+      body: {
+        items: [
+          {
+            id,
+            title: title,
+            currency_id: "ARS",
+            category_id: "art",
+            quantity: quantity,
+            unit_price: price,
+          },
+        ],
+        back_urls: {
+          success: "http://localhost:3000/trolley/purchase-completed",
+          failure: "http://localhost:3000/",
+          pending: "http://localhost:3000/loading",
+        },
+        auto_return: "approved",
+
+        /* payment_methods: {
+          excluded_payment_methods: [],
+          excluded_payment_types: [],
+          installments: 1,
+        },
+        notification_url: "https://www.your-site.com/ipn", */
+      },
+    });
+
+    res.send(response);
+  } catch (error) {
+    console.error("Error al crear preferencia:", error);
+    res.status(500).send("Error al crear preferencia");
+  }
+};
 
 exports.createOrder = async (req, res) => {
   const { cartCourses, orderId } = req.body;
+  console.log("HOLAAA");
 
   try {
     //Busca el modelo de la orden actual en la DB
@@ -13,35 +58,48 @@ exports.createOrder = async (req, res) => {
 
     // Obtén todos los items del carrito asociados a la orden
     const cartItems = await Cart.findOne({ userId: orderResponse.userId });
+    //console.log("========CART", cartItems);
 
     // Obtén descuento del carrito asociados a la orden (de haberlo aplicado)
     const totalDiscount = cartItems.discount;
 
-    console.log("DESCEUNTO-------------------------------------", totalDiscount);
-    console.log("PRECIO-------------------------------------", cartItems.totalDiscount);
-    console.log("PRECIO2-------------------------------------", cartItems.totalAmount);
+    console.log(
+      "DESCEUNTO-------------------------------------",
+      totalDiscount
+    );
+    console.log(
+      "PRECIO-------------------------------------",
+      cartItems.totalDiscount
+    );
+    console.log(
+      "PRECIO2-------------------------------------",
+      cartItems.totalAmount
+    );
 
     //Genera configuracion de Mercado Pago
-    const client = new MercadoPagoConfig({ accessToken: process.env.ACCESS_TOKEN_MP });
+    const client = new MercadoPagoConfig({
+      accessToken: process.env.ACCESS_TOKEN_MP,
+    });
 
     try {
       //Se crea un arreglo para guardar las ordenes creadas en base a la cantidad de cursos en el carrito
       const courses = [];
-      cartCourses.forEach(element => {
+      cartCourses.forEach((element) => {
         //Manejando decuentos
         const price = parseFloat(element.coursePrice);
         const discount = (price * totalDiscount) / 100;
-        const finalDiscount = price - discount; 
+        const finalDiscount = price - discount;
         const order = {
           id: element._id,
           title: element.courseLongTitle,
           quantity: 1,
-          unit_price: totalDiscount ? finalDiscount : price
+          unit_price: totalDiscount ? finalDiscount : price,
         };
         courses.push(order);
       });
 
       //Preferencias y creacion para Mercado Pago
+      //console.log(courses);
       const preference = new Preference(client);
       const result = await preference.create({
         body: {
@@ -73,12 +131,12 @@ exports.updateOrder = async (req, res) => {
   const { userId } = req.body;
 
   try {
-    const order = await Order.findOne({userId});
-    !order && res.status(404).send("No Se Encontro Orden")
+    const order = await Order.findOne({ userId });
+    !order && res.status(404).send("No Se Encontro Orden");
 
-    res.status(200).send(order)
+    res.status(200).send(order);
   } catch (error) {
     console.error(error);
-    res.sendStatus(500)
+    res.sendStatus(500);
   }
-}
+};
--- api/routes/paymentMp.routes.js
+++ api/routes/paymentMp.routes.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { createOrder, updateOrder } = require("../controllers/paymentMp.controller");
+const {
+  createOrder,
+  updateOrder,
+  createOrderIvan,
+} = require("../controllers/paymentMp.controller");
 
 router.post("/create-order", createOrder);
 router.get("/success", (req, res) => res.send("success"));
 router.get("/failure", (req, res) => res.send("failure"));
 router.get("/pending", (req, res) => res.send("pending"));
 router.put("/updateOrder", updateOrder);
+router.post("/create-order-ivan", createOrderIvan);
 
 module.exports = router;
